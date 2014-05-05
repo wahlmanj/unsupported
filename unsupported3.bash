bfb15a46ba26285418c8d280a35071688dc65cfb
wahlmanj <wahlman.j@gmail.com>
2014-05-05 02:06:43 -0700

    Update unsupported3.bash

--- unsupported3.bash
+++ unsupported3.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp -R /applications/unsupportedappstore.bundle /Users/__USERNAME__/Library/Application\ Support/Plex\ Media\ Server/Plug-ins
-## rm -Rf /applications/unsupported
-##rm /usr/bin/unsupported.bash
-## rm /usr/bin/unsupported2.bash
-##rm /usr/bin/unsupported3.bash
+rm -Rf /applications/unsupported
+rm /usr/bin/unsupported.bash
+rm /usr/bin/unsupported2.bash
+rm /usr/bin/unsupported3.bash
+rm -Rf /applications/unsupportedappstore.bundle
