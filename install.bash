84b1fa6344a42a50f42cdb9785d01aeb6d6509fd
wahlmanj <wahlman.j@gmail.com>
2014-05-05 00:21:11 -0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd /applications/unsupported
+
 #current user
 whoami=${USER}
 
 ## replace __USERNAME__in default setup.bash
 ## save directly to the /applications/unsupported folder
-sed -e "s/__USERNAME__/$whoami/" > /applications/unsupported/setup.bash
+sed -e "s/__USERNAME__/$whoami/" > /applications/unsupported/unsupported.bash
