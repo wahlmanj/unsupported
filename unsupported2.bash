ffa9d4242cfb195944ad7ac8900173eedf2babaa
wahlmanj <wahlman.j@gmail.com>
2014-05-05 01:20:18 -0700

    Update unsupported2.bash

--- unsupported2.bash
+++ unsupported2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /applications/unsupported
+cd /usr/bin
 DefaultPath=${PWD}
 
 #current user
@@ -8,4 +8,4 @@ whoami=${USER}
 
 ## replace __USERNAME__in default setup.bash
 ## save directly to the /applications/unsupported folder
-sed -e "s/__USERNAME__/$whoami/" "${DefaultPath}/setup.bash" > /applications/unsupported/copy.bash
+sed -e "s/__USERNAME__/$whoami/" "${DefaultPath}/unsupported3.bash" > /applications/unsupported/copy.bash
