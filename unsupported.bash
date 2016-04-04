7be9b761a8dbd01c586eb9c8f5f62979aa276aba
wahlmanj <wahlman.j@gmail.com>
2016-04-04 00:18:06 -0400

    Update unsupported.bash

--- unsupported.bash
+++ unsupported.bash
@@ -1,8 +1,8 @@
 cd /applications/unsupported
 
-cp unsupported2.bash /usr/bin
-cp unsupported3.bash /usr/bin
-cp unsupported4.bash /usr/bin
-chmod +x /usr/bin/unsupported2.bash
-chmod +x /usr/bin/unsupported3.bash
-chmod +x /usr/bin/unsupported3.bash
+cp unsupported2.bash /usr/local/bin
+cp unsupported3.bash /usr/local/bin
+cp unsupported4.bash /usr/local/bin
+chmod +x /usr/local/bin/unsupported2.bash
+chmod +x /usr/local/bin/unsupported3.bash
+chmod +x /usr/local/bin/unsupported3.bash
